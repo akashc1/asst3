825da001f72e7e372e8e56e1218c9f68fd379068
Ubuntu <ubuntu@ip-172-31-44-68.us-west-2.compute.internal>
2022-11-08 01:57:30 +0000

    Finish part 1, WIP part 2

--- scan/scan.cu
+++ scan/scan.cu
@@ -42,6 +42,34 @@ static inline int nextPow2(int n) {
 // Also, as per the comments in cudaScan(), you can implement an
 // "in-place" scan, since the timing harness makes a copy of input and
 // places it in result
+__global__ void upsweep_kernel(int *output, int N, int two_d, int two_dplus1)
+{
+        int index = blockIdx.x * blockDim.x + threadIdx.x;
+        index = index * two_dplus1;
+        if (index < N) {
+            output[index + two_dplus1 - 1] += output[index + two_d - 1];
+        }
+}
+
+__global__ void downsweep_kernel(int *output, int N, int two_d, int two_dplus1)
+{
+        int index = blockIdx.x * blockDim.x + threadIdx.x;
+        index = index * two_dplus1;
+        if (two_d == N / 2 and index == 0){
+            output[N-1] = 0;
+        }
+        __syncthreads();
+        if (index < N) {
+            if (index + two_d - 1 >= N)
+                output[index + two_d - 1] = 0;
+            if (index + two_dplus1 - 1 >= N)
+                output[index + two_dplus1 - 1] = 0;
+            int tmp = output[index + two_d - 1];
+            output[index + two_d - 1] = output[index + two_dplus1 - 1];
+            output[index + two_dplus1 - 1] += tmp;
+        }
+}
+
 void exclusive_scan(int* input, int N, int* result)
 {
 
@@ -53,7 +81,20 @@ void exclusive_scan(int* input, int N, int* result)
     // on the CPU.  Your implementation will need to make multiple calls
     // to CUDA kernel functions (that you must write) to implement the
     // scan.
-
+    for (int two_d = 1; two_d <= N/2; two_d *= 2) {
+        int two_dplus1 = two_d * 2;
+        int num_threads = N / two_dplus1;
+        int blocks = (num_threads + THREADS_PER_BLOCK - 1) / THREADS_PER_BLOCK;
+        upsweep_kernel<<<blocks, THREADS_PER_BLOCK>>>(result, N, two_d, two_dplus1);
+        cudaDeviceSynchronize();
+    }
+    for (int two_d = N / 2; two_d >= 1; two_d /= 2) {
+        int two_dplus1 = two_d * 2;
+        int num_threads = N / two_dplus1;
+        int blocks = (num_threads + THREADS_PER_BLOCK - 1) / THREADS_PER_BLOCK;
+        downsweep_kernel<<<blocks, THREADS_PER_BLOCK>>>(result, N, two_d, two_dplus1);
+        cudaDeviceSynchronize();
+    }
 
 }
 
